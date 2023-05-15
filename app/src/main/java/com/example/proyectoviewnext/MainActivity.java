7e07602e3514568ad3b70a908ca73960c562ce72
Juanma Gutiérrez <juanmanuelgutierrezmellado@gmail.com>
2023-05-15 13:41:27 +0200

    add Menú toolbar

--- app/src/main/java/com/example/proyectoviewnext/MainActivity.java
+++ app/src/main/java/com/example/proyectoviewnext/MainActivity.java
@@ -1,13 +1,22 @@
 package com.example.proyectoviewnext;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.zip.Inflater;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -23,7 +32,19 @@ public class MainActivity extends AppCompatActivity {
         init();
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
     public void init() {
+        // Menu
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        toolbar.setTitle(R.string.invoices_list_title);
+        setSupportActionBar(toolbar);
+
         // Asignamos a la lista la vista list_view_invoices
         recycler_view_list = (RecyclerView) findViewById(R.id.list_view_invoices);
         if (recycler_view_list != null) {
@@ -37,8 +58,31 @@ public class MainActivity extends AppCompatActivity {
         invoice_list.add(new Invoice("10/10/2020", "Pendiente", 30.10));
         invoice_list.add(new Invoice("8/10/2020", "Pagado", 5.75));
         invoice_list.add(new Invoice("6/10/2020", "", 125.50));
+        invoice_list.add(new Invoice("12/10/2020", "Pendiente", 50.23));
+        invoice_list.add(new Invoice("10/10/2020", "Pendiente", 30.10));
+        invoice_list.add(new Invoice("8/10/2020", "Pagado", 5.75));
+        invoice_list.add(new Invoice("6/10/2020", "", 125.50));
+        invoice_list.add(new Invoice("12/10/2020", "Pendiente", 50.23));
+        invoice_list.add(new Invoice("10/10/2020", "Pendiente", 30.10));
+        invoice_list.add(new Invoice("8/10/2020", "Pagado", 5.75));
+        invoice_list.add(new Invoice("6/10/2020", "", 125.50));
 
         InvoicesAdapter adapter = new InvoicesAdapter(invoice_list);
         recycler_view_list.setAdapter(adapter);
+    }
+
+    // Diálogo al hacer click en los items
+    public void onItemClick(View v) {
+        AlertDialog.Builder alert_dialog = new AlertDialog.Builder(this);
+        alert_dialog
+                .setTitle(R.string.alert_title)
+                .setMessage(R.string.alert_info)
+                .setPositiveButton(R.string.close_button, (dialog, which) -> dialog.dismiss());
+        alert_dialog.create().show();
+
+    }
+
+    public void openFilter(MenuItem menu_item) {
+        Toast.makeText(this, "OpenFilter", Toast.LENGTH_SHORT).show();
     }
 }
